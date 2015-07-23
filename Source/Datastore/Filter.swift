4e560ae7f92766559a9c6c37afde5c5e8bbb0577
Bruno Farache <bruno.farache@liferay.com>
2015-07-23 11:18:16 -0300

    Add new filter types

--- Source/Datastore/Filter.swift
+++ Source/Datastore/Filter.swift
@@ -1,8 +1,15 @@
 import Foundation
 
-public class Filter {
+public class Filter : Printable {
 
 	public var filter = [String: [String: AnyObject]]()
+
+	public var description: String {
+		let data = NSJSONSerialization.dataWithJSONObject(
+			filter, options: NSJSONWritingOptions.allZeros, error: nil)!
+
+		return NSString(data: data,  encoding: NSUTF8StringEncoding)! as String
+	}
 
 	init(field: String, op: String = "=", value: AnyObject) {
 		filter[field] = [
@@ -11,8 +18,32 @@ public class Filter {
 		]
 	}
 
+	public static func equal(field: String, _ value: AnyObject) -> Filter {
+		return Filter(field: field, value: value)
+	}
+
 	public static func gt(field: String, _ value: AnyObject) -> Filter {
 		return Filter(field: field, op: ">", value: value)
+	}
+
+	public static func gte(field: String, _ value: AnyObject) -> Filter {
+		return Filter(field: field, op: ">=", value: value)
+	}
+
+	public static func regex(field: String, _ value: AnyObject) -> Filter {
+		return Filter(field: field, op: "~", value: value)
+	}
+
+	public static func lt(field: String, _ value: AnyObject) -> Filter {
+		return Filter(field: field, op: "<", value: value)
+	}
+
+	public static func lte(field: String, _ value: AnyObject) -> Filter {
+		return Filter(field: field, op: "<=", value: value)
+	}
+
+	public static func notEqual(field: String, _ value: AnyObject) -> Filter {
+		return Filter(field: field, op: "!=", value: value)
 	}
 
 }
---
+++ Test/Datastore/FilterTest.swift
@@ -0,0 +1,68 @@
+import XCTest
+
+class FilterTest : BaseTest {
+
+	func testCustom() {
+		let filter = Filter(field:"age", op: ">", value: 12)
+
+		XCTAssertEqual(
+			"{\"age\":{\"operator\":\">\",\"value\":12}}", filter.description)
+	}
+
+	func testDefaultOperator() {
+		let filter = Filter(field:"age", value: 12)
+
+		XCTAssertEqual(
+			"{\"age\":{\"operator\":\"=\",\"value\":12}}", filter.description)
+	}
+
+	func testEqual() {
+		let filter = Filter.equal("age", 12)
+
+		XCTAssertEqual(
+			"{\"age\":{\"operator\":\"=\",\"value\":12}}", filter.description)
+	}
+
+	func testGt() {
+		let filter = Filter.gt("age", 12)
+
+		XCTAssertEqual(
+			"{\"age\":{\"operator\":\">\",\"value\":12}}", filter.description)
+	}
+
+	func testGte() {
+		let filter = Filter.gte("age", 12)
+
+		XCTAssertEqual(
+			"{\"age\":{\"operator\":\">=\",\"value\":12}}", filter.description)
+	}
+
+	func testLt() {
+		let filter = Filter.lt("age", 12)
+
+		XCTAssertEqual(
+			"{\"age\":{\"operator\":\"<\",\"value\":12}}", filter.description)
+	}
+
+	func testLte() {
+		let filter = Filter.lte("age", 12)
+
+		XCTAssertEqual(
+			"{\"age\":{\"operator\":\"<=\",\"value\":12}}", filter.description)
+	}
+
+	func testNotEqual() {
+		let filter = Filter.notEqual("age", 12)
+
+		XCTAssertEqual(
+			"{\"age\":{\"operator\":\"!=\",\"value\":12}}", filter.description)
+	}
+
+	func testRegex() {
+		let filter = Filter.regex("age", 12)
+
+		XCTAssertEqual(
+			"{\"age\":{\"operator\":\"~\",\"value\":12}}", filter.description)
+	}
+
+}
