21b3196855551d4ab4ea2700e5e0cd73a6e37d95
Bruno Farache <bruno.farache@liferay.com>
2016-03-24 18:08:47 -0300

    Add test to check if all requests have the Authorization header

--- Test/Integration/AuthTest.swift
+++ Test/Integration/AuthTest.swift
@@ -8,6 +8,8 @@ class AuthTest : XCTestCase {
 	var server: String!
 	var username: String!
 
+	let basicHeader = "Basic YnJ1bm8uZmFyYWNoZUBsaWZlcmF5LmNvbTp0ZXN0"
+
 	override func setUp() {
 		let bundle = NSBundle(identifier: "com.liferay.Launchpad.Tests")
 		let file = bundle!.pathForResource("settings", ofType: "plist")
@@ -33,6 +35,116 @@ class AuthTest : XCTestCase {
 			.get()
 			.then { response in
 				XCTAssertEqual(response.statusCode, 200)
+				expectation.fulfill()
+			}
+			.done()
+
+		wait()
+	}
+
+	func testGetWithAuth() {
+		let expectation = expect("auth")
+		let transport = MockTransport()
+
+		Launchpad
+			.url(server)
+			.path(path)
+			.auth(BasicAuth(username, "test"))
+			.use(transport)
+			.get()
+			.then { response in
+				XCTAssertEqual(
+					self.basicHeader,
+					transport.request!.headers["Authorization"])
+
+				expectation.fulfill()
+			}
+			.done()
+
+		wait()
+	}
+
+	func testDeleteWithAuth() {
+		let expectation = expect("auth")
+		let transport = MockTransport()
+
+		Launchpad
+			.url(server)
+			.path(path)
+			.auth(BasicAuth(username, "test"))
+			.use(transport)
+			.delete()
+			.then { response in
+				XCTAssertEqual(
+					self.basicHeader,
+					transport.request!.headers["Authorization"])
+
+				expectation.fulfill()
+			}
+			.done()
+
+		wait()
+	}
+
+	func testPatchWithAuth() {
+		let expectation = expect("auth")
+		let transport = MockTransport()
+
+		Launchpad
+			.url(server)
+			.path(path)
+			.auth(BasicAuth(username, "test"))
+			.use(transport)
+			.patch(nil)
+			.then { response in
+				XCTAssertEqual(
+					self.basicHeader,
+					transport.request!.headers["Authorization"])
+
+				expectation.fulfill()
+			}
+			.done()
+
+		wait()
+	}
+
+	func testPostWithAuth() {
+		let expectation = expect("auth")
+		let transport = MockTransport()
+
+		Launchpad
+			.url(server)
+			.path(path)
+			.auth(BasicAuth(username, "test"))
+			.use(transport)
+			.post(nil)
+			.then { response in
+				XCTAssertEqual(
+					self.basicHeader,
+					transport.request!.headers["Authorization"])
+
+				expectation.fulfill()
+			}
+			.done()
+
+		wait()
+	}
+
+	func testPutWithAuth() {
+		let expectation = expect("auth")
+		let transport = MockTransport()
+
+		Launchpad
+			.url(server)
+			.path(path)
+			.auth(BasicAuth(username, "test"))
+			.use(transport)
+			.put(nil)
+			.then { response in
+				XCTAssertEqual(
+					self.basicHeader,
+					transport.request!.headers["Authorization"])
+
 				expectation.fulfill()
 			}
 			.done()
@@ -78,9 +190,23 @@ class AuthTest : XCTestCase {
 		let auth = BasicAuth(username, "test")
 		auth.authenticate(request)
 
-		XCTAssertEqual(
-			"Basic YnJ1bm8uZmFyYWNoZUBsaWZlcmF5LmNvbTp0ZXN0",
-			request.headers["Authorization"])
+		XCTAssertEqual(basicHeader, request.headers["Authorization"])
+	}
+
+}
+
+class MockTransport : Transport {
+
+	var request : Request?
+
+	func send(
+		request: Request, success: (Response -> ())?,
+		failure: (NSError -> ())?) {
+
+		self.request = request
+
+		let data = "".dataUsingEncoding(NSUTF8StringEncoding)
+		success!(Response(statusCode: 200, headers: [:], body: data!))
 	}
 
 }
