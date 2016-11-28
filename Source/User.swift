ef845a492363ad3b60d375cb02e0d235101d8284
Victor Galan <victor.galan@liferay.com>
2016-11-28 10:17:16 +0100

    Add user class

---
+++ Source/User.swift
@@ -0,0 +1,28 @@
+//
+//  User.swift
+//  wedeployPoC
+//
+//  Created by Victor Galán on 22/11/16.
+//  Copyright © 2016 liferay. All rights reserved.
+//
+
+import Foundation
+
+public struct User {
+	public let email: String
+	public let name: String?
+	public let id: String
+
+	public init(json: Dictionary<String, AnyObject>) {
+
+		email = json["email"] as! String
+		name = json["name"] as? String
+		id = json["id"] as! String
+	}
+
+	public init(email: String, name: String) {
+		self.email = email
+		self.name = name
+		self.id = ""
+	}
+}
