01222d3102ebc8aea43a246ee61e6fd717fa522a
Victor Galan <victor.galan@liferay.com>
2016-11-29 16:29:40 +0100

    Save session data in Wedeploy instance

--- Source/WeDeploy.swift
+++ Source/WeDeploy.swift
@@ -8,15 +8,49 @@
 
 import Foundation
 
+struct AuthSession {
+	internal var currentUser: User?
+	internal var currentAuth: Auth?
+
+	let url: String
+
+	init(_ url: String) {
+		self.url = url
+	}
+
+}
+
 
 public class WeDeploy : RequestBuilder {
+
+	internal static var authSession: AuthSession?
+
+	override var authorization: Auth? {
+		set {
+			self.authorization = newValue
+		}
+		get {
+			return WeDeploy.authSession?.currentAuth
+		}
+	}
 
 	override public class func url(_ url: String) -> WeDeploy {
 		return WeDeploy(url)
 	}
 
-	public func auth() -> WeDeployAuth {
-		return WeDeployAuth(self.url)
+	public class func auth(_ url: String) -> WeDeployAuth {
+		authSession = AuthSession(url)
+
+		return WeDeployAuth(url)
+	}
+
+	public class func auth() -> WeDeployAuth {
+		precondition(authSession != nil, "you have to initialize auth module")
+
+		return WeDeployAuth(
+				authSession!.url,
+				user: authSession!.currentUser,
+				authorization: authSession!.currentAuth)
 	}
 
 	public func data() -> Self {
