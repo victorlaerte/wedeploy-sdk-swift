85f4c2261d2c4054054909067c7072a88b9e0354
Victor Galan <victor.galan@liferay.com>
2016-11-29 09:54:39 +0100

    Add Wedeploy class that extend from requestbuilder

---
+++ Source/WeDeploy.swift
@@ -0,0 +1,26 @@
+//
+//  WeDeploy.swift
+//  Launchpad
+//
+//  Created by Victor Galán on 29/11/16.
+//  Copyright © 2016 Liferay Inc. All rights reserved.
+//
+
+import Foundation
+
+
+public class WeDeploy : RequestBuilder {
+
+
+	public func authModule() -> Self {
+		return self
+	}
+
+	public func dataModule() -> Self {
+		return self
+	}
+
+	public func emailModule() -> Self {
+		return self
+	}
+}
