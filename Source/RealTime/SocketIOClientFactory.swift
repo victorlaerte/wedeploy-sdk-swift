29151adec037a31c87268cd6d8131278c0f87931
Victor Galan <victor.galan@liferay.com>
2016-12-28 16:56:12 +0100

    Update socket io client factory

--- Source/RealTime/SocketIOClientFactory.swift
+++ Source/RealTime/SocketIOClientFactory.swift
@@ -17,47 +17,48 @@ import SocketIO
 
 public class SocketIOClientFactory {
 
-//	class func create(
-//			url: String, params: [URLQueryItem]? = [],
-//			options: inout SocketIOClientConfiguration)
-//		-> SocketIOClient {
-//
-//		if (!options.contains(.forceNew(false))) {
-//			options.insert(.forceNew(true))
-//		}
-//
-//		let url = parseURL(url: url, params: params)
-//
-//		options.insert(.connectParams(["EIO": "3", "url": url.query]))
-//		options.insert(.path(url.path))
-//
-//		let socket = SocketIOClient(socketURL: url, config: options)
-//		socket.connect()
-//
-//		return socket
-//	}
-//
-//	class func parseURL(url: String, params: [URLQueryItem]? = [])
-//		-> (host: String, path: String, query: String) {
-//
-//		let URL = NSURLComponents(string: url)!
-//		URL.queryItems = params
-//
-//		let host = URL.host ?? ""
-//		var port = ""
-//
-//		if let p = URL.port, URL.port != 80 {
-//			port = ":\(p)"
-//		}
-//
-//		let path = URL.path!
-//		var query = path
-//
-//		if let q = URL.query {
-//			query = "\(path)?\(q)"
-//		}
-//
-//		return ("\(host)\(port)", path, query)
-//	}
+	public class func create(
+			url: String, params: [URLQueryItem]? = [],
+			options: inout SocketIOClientConfiguration)
+		-> SocketIOClient {
+
+		if (!options.contains(.forceNew(false))) {
+			options.insert(.forceNew(true))
+		}
+
+		let urlComponents = parseURL(url: url, params: params)
+
+		options.insert(.connectParams(["EIO": "3", "url": urlComponents.query]))
+		options.insert(.path(urlComponents.path))
+
+		let socket = SocketIOClient(socketURL: URL(string: urlComponents.host)!, config: options)
+		socket.connect()
+
+		return socket
+	}
+
+	class func parseURL(url: String, params: [URLQueryItem]? = [])
+		-> (host: String, path: String, query: String) {
+
+		let URL = NSURLComponents(string: url)!
+		URL.queryItems = params
+
+		let host = URL.host ?? ""
+		var port = ""
+
+		if let p = URL.port, URL.port != 80 {
+			port = ":\(p)"
+		}
+
+		let path = URL.path!
+		let scheme = "\(URL.scheme ?? "http")://"
+		var query = path
+
+		if let q = URL.query {
+			query = "\(path)?\(q)"
+		}
+
+		return ("\(scheme)\(host)\(port)", path, query)
+	}
 
 }
