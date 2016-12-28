7e96ee6549efa583e8eab72882ba7b13886ffe01
Victor Galan <victor.galan@liferay.com>
2016-12-28 17:05:07 +0100

    Refactor url test

--- Test/RealTime/WatchTest.swift
+++ Test/RealTime/WatchTest.swift
@@ -1,92 +1,34 @@
-@testable import Launchpad
-import Socket_IO_Client_Swift
+@testable import WeDeploy
+import SocketIO
 import XCTest
 
 class WatchTest : BaseTest {
 
+	func testParseURLScheme() {
+		let urlHttp = SocketIOClientFactory.parseURL(url: "http://domain.com")
+		let urlHttps = SocketIOClientFactory.parseURL(url: "https://domain.com")
+
+		XCTAssertEqual("http://domain.com", urlHttp.host)
+		XCTAssertEqual("https://domain.com", urlHttps.host)
+	}
+
 	func testParseURL() {
-		let url = SocketIOClientFactory.parseURL("http://domain:8080/path/a")
-		XCTAssertEqual("domain:8080", url.host)
+		let url = SocketIOClientFactory.parseURL(url: "http://domain:8080/path/a")
+		XCTAssertEqual("http://domain:8080", url.host)
 		XCTAssertEqual("/path/a", url.path)
 	}
 
 	func testParseURL_With_Query() {
 		let params = [
-			NSURLQueryItem(name: "param1", value: "value1"),
-			NSURLQueryItem(name: "param2", value: "value2")
+			URLQueryItem(name: "param1", value: "value1"),
+			URLQueryItem(name: "param2", value: "value2")
 		]
 
-		let url = SocketIOClientFactory.parseURL(
-			"http://domain:8080/path/a", params: params)
+		let url = SocketIOClientFactory.parseURL(url: "http://domain:8080/path/a", params: params)
 
-		XCTAssertEqual("domain:8080", url.host)
+		XCTAssertEqual("http://domain:8080", url.host)
 		XCTAssertEqual("/path/a", url.path)
 		XCTAssertEqual("/path/a?param1=value1&param2=value2", url.query)
-	}
-
-	func testWatch() {
-		let expectation = expect("watch")
-		let socket = launchpad.watch()
-
-		guard let book = booksToAdd.first else {
-			return
-		}
-
-		socket.on("connect", callback: { _ in
-			self.launchpad
-				.post(book)
-			.done()
-		})
-
-		socket.on("changes", callback: { data, _ in
-			guard let books = data[0] as? [[String: AnyObject]] else {
-				XCTFail("Could not parse books")
-				expectation.fulfill()
-				return
-			}
-
-			XCTAssertEqual(books.count, 3)
-			expectation.fulfill()
-		})
-
-		wait(5)
-	}
-
-	func testWatch_With_Query() {
-		let expectation = expect("watch")
-
-		guard var book = books.first else {
-			return
-		}
-
-		book["title"] = "La fiesta del chivo"
-
-		let id = book["id"] as! String
-
-		let socket = launchpad
-			.filter("year", book["year"]!)
-			.watch()
-
-		socket.on("connect", callback: { _ in
-			self.launchpad
-				.path("/\(id)")
-				.put(book)
-			.done()
-		})
-
-		socket.on("changes", callback: { data, _ in
-			guard let books = data[0] as? [[String: AnyObject]] else {
-				XCTFail("Could not parse books")
-				expectation.fulfill()
-				return
-			}
-
-			XCTAssertEqual(books.count, 1)
-			self.assertBook(book, book: books[0])
-			expectation.fulfill()
-		})
-
-		wait(5)
 	}
 
 }
