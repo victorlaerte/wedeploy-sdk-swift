577fdb21e0e87ce824765a376cfcfd9cc8699959
Victor Galan <victor.galan@liferay.com>
2017-01-03 11:35:06 +0100

    Add data operators test

--- Test/Integration/WeDeployDataTest.swift
+++ Test/Integration/WeDeployDataTest.swift
@@ -8,10 +8,10 @@
 
 @testable import WeDeploy
 import later
-import RxSwift
 import XCTest
 
-class WeDeployDataTest: BaseTest {
+class WeDeployDataCreationTest: BaseTest {
+
 
 	func testCreateResource() {
 		let resource: [String: AnyObject] = [
@@ -49,7 +49,7 @@ class WeDeployDataTest: BaseTest {
 			"description": "another description" as AnyObject
 		]
 
-		let expect = expectation(description: "resource created")
+		let expect = expectation(description: "resources created")
 
 		executeAuthenticated {
 			WeDeploy.data(self.dataModuleUrl)
@@ -64,6 +64,224 @@ class WeDeployDataTest: BaseTest {
 
 					expect.fulfill()
 			}
+		}
+
+		waitForExpectations(timeout: 10, handler: nil)
+	}
+
+}
+
+
+class WeDeployDataTest: BaseTest {
+
+	let collectionName = "things"
+
+	let data: [[String: AnyObject]] = [
+		["name": "aname1" as AnyObject, "yearsOld": "10" as AnyObject],
+		["name": "aname2" as AnyObject, "yearsOld": "15" as AnyObject],
+		["name": "aname3" as AnyObject, "yearsOld": "20" as AnyObject],
+		["name": "aname4" as AnyObject, "yearsOld": "25" as AnyObject],
+		["name": "aname5" as AnyObject, "yearsOld": "35" as AnyObject],
+		["name": "aname6" as AnyObject, "yearsOld": "45" as AnyObject],
+		["name": "aname7" as AnyObject, "yearsOld": "55" as AnyObject]
+	]
+
+	override func setUp() {
+		super.setUp()
+
+		let exp = expect(description: "filling data")
+
+		WeDeploy.data(self.dataModuleUrl)
+			.create(resource: collectionName, object: data)
+			.done { _ ,_ in
+				exp.fulfill()
+			}
+
+		waitForExpectations(timeout: 10, handler: nil)
+	}
+
+	override func tearDown() {
+		super.tearDown()
+
+		let exp = expect(description: "empty data")
+
+		WeDeploy.data(self.dataModuleUrl)
+			.delete(collectionOrResourcePath: collectionName)
+			.done { _ ,_ in
+				exp.fulfill()
+		}
+
+		waitForExpectations(timeout: 10, handler: nil)
+	}
+
+	func testWhereOperator() {
+		let exp = expect(description: "where")
+
+		WeDeploy
+			.data(self.dataModuleUrl)
+			.where(field: "yearsOld", op: ">", value: "14")
+			.get(resourcePath: collectionName)
+			.done {	items, error in
+				XCTAssertNil(error)
+				XCTAssertNotNil(items)
+
+				XCTAssertEqual(items?.count ?? 0, 6)
+
+				exp.fulfill()
+			}
+
+		waitForExpectations(timeout: 10, handler: nil)
+	}
+
+	func testAnyOperator() {
+		let exp = expect(description: "where")
+
+		WeDeploy
+			.data(self.dataModuleUrl)
+			.where(field: "yearsOld", op: ">", value: "14")
+			.any(field: "yearsOld", value: ["20","25"])
+			.get(resourcePath: collectionName)
+			.done {	items, error in
+				XCTAssertNil(error)
+				XCTAssertNotNil(items)
+
+				XCTAssertEqual(items?.count ?? 0, 2)
+
+				exp.fulfill()
+		}
+
+		waitForExpectations(timeout: 10, handler: nil)
+	}
+
+	func testCountOperator() {
+		let exp = expect(description: "where")
+
+		WeDeploy
+			.data(self.dataModuleUrl)
+			.where(field: "yearsOld", op: ">", value: "14")
+			.getCount(resourcePath: collectionName)
+			.done {	count, error in
+				XCTAssertNil(error)
+				XCTAssertNotNil(count)
+
+				XCTAssertEqual(count ?? 0, 6)
+
+				exp.fulfill()
+		}
+
+		waitForExpectations(timeout: 10, handler: nil)
+	}
+
+	func testLimitOperator() {
+		let exp = expect(description: "where")
+
+		WeDeploy
+			.data(self.dataModuleUrl)
+			.where(field: "yearsOld", op: ">", value: "14")
+			.limit(1)
+			.get(resourcePath: collectionName)
+			.done {	items, error in
+				XCTAssertNil(error)
+				XCTAssertNotNil(items)
+
+				XCTAssertEqual(items?.count ?? 0, 1)
+
+				exp.fulfill()
+		}
+
+		waitForExpectations(timeout: 10, handler: nil)
+	}
+
+	func testOrderAscOperator() {
+		let exp = expect(description: "where")
+
+		WeDeploy
+			.data(self.dataModuleUrl)
+			.where(field: "yearsOld", op: ">", value: "14")
+			.orderBy(field: "yearsOld", order: .ASC)
+			.get(resourcePath: collectionName)
+			.done {	items, error in
+				XCTAssertNil(error)
+				XCTAssertNotNil(items)
+
+				XCTAssertEqual(items?.count ?? 0, 6)
+
+				XCTAssertEqual(items?[0]["yearsOld"] as? String ?? "0", "15")
+				XCTAssertEqual(items?[1]["yearsOld"] as? String ?? "0", "20")
+				XCTAssertEqual(items?[2]["yearsOld"] as? String ?? "0", "25")
+
+				exp.fulfill()
+		}
+
+		waitForExpectations(timeout: 10, handler: nil)
+	}
+
+	func testOrderDescOperator() {
+		let exp = expect(description: "where")
+
+		WeDeploy
+			.data(self.dataModuleUrl)
+			.where(field: "yearsOld", op: ">", value: "14")
+			.orderBy(field: "yearsOld", order: .DESC)
+			.get(resourcePath: collectionName)
+			.done {	items, error in
+				XCTAssertNil(error)
+				XCTAssertNotNil(items)
+
+				XCTAssertEqual(items?.count ?? 0, 6)
+
+				XCTAssertEqual(items?[0]["yearsOld"] as? String ?? "0", "55")
+				XCTAssertEqual(items?[1]["yearsOld"] as? String ?? "0", "45")
+				XCTAssertEqual(items?[2]["yearsOld"] as? String ?? "0", "35")
+
+				exp.fulfill()
+		}
+
+		waitForExpectations(timeout: 10, handler: nil)
+	}
+
+	func testOffsetOperator() {
+		let exp = expect(description: "where")
+
+		WeDeploy
+			.data(self.dataModuleUrl)
+			.where(field: "yearsOld", op: ">", value: "14")
+			.orderBy(field: "yearsOld", order: .ASC)
+			.offset(1)
+			.get(resourcePath: collectionName)
+			.done {	items, error in
+				XCTAssertNil(error)
+				XCTAssertNotNil(items)
+
+				XCTAssertEqual(items?.count ?? 0, 5)
+
+				XCTAssertEqual(items?[0]["yearsOld"] as? String ?? "0", "20")
+
+				exp.fulfill()
+		}
+
+		waitForExpectations(timeout: 10, handler: nil)
+	}
+
+	func testOffsetLimitOperator() {
+		let exp = expect(description: "where")
+
+		WeDeploy
+			.data(self.dataModuleUrl)
+			.where(field: "yearsOld", op: ">", value: "14")
+			.orderBy(field: "yearsOld", order: .ASC)
+			.limit(1)
+			.offset(1)
+			.get(resourcePath: collectionName)
+			.done {	items, error in
+				XCTAssertNil(error)
+				XCTAssertNotNil(items)
+
+				XCTAssertEqual(items?.count ?? 0, 1)
+
+				XCTAssertEqual(items?[0]["yearsOld"] as? String ?? "0", "20")
+
+				exp.fulfill()
 		}
 
 		waitForExpectations(timeout: 10, handler: nil)
