97e37ec305aac815a4b3cf5ca65a3597d5b6deb8
Victor Galan <victor.galan@liferay.com>
2017-03-24 09:55:06 +0100

    Add missing email params and make it fluent

--- Source/Services/WeDeployEmail.swift
+++ Source/Services/WeDeployEmail.swift
@@ -18,19 +18,62 @@ import RxSwift
 
 public class WeDeployEmail : WeDeployService {
 
-	public func sendEmail(from: String, to: String, subject: String? = "", body: String) -> Promise<String> {
-		return RequestBuilder
+	var params: [(name: String, value: String)] = []
+
+	public func from(_ from: String) -> Self {
+		params.append(("from", from))
+		return self
+	}
+
+	public func to(_ to: String) -> Self {
+		params.append(("to", to))
+		return self
+	}
+
+	public func subject(_ subject: String) -> Self {
+		params.append(("subject", subject))
+		return self
+	}
+
+	public func message(_ message: String) -> Self {
+		params.append(("message", message))
+		return self
+	}
+
+	public func priority(_ priority: Int) -> Self {
+		params.append(("priority", "\(priority)"))
+		return self
+	}
+
+	public func replyTo(_ replyTo: String) -> Self {
+		params.append(("replyTo", replyTo))
+		return self
+	}
+
+	public func cc(_ cc: String) -> Self {
+		params.append(("cc", cc))
+		return self
+	}
+
+	public func bcc(_ bcc: String) -> Self {
+		params.append(("bcc", bcc))
+		return self
+	}
+
+	public func send() -> Promise<String> {
+		var builder = RequestBuilder
 				.url(self.url)
 				.path("/emails")
-				.form(name: "from", value: from)
-				.form(name: "to", value: to)
-				.form(name: "subject", value: subject!)
-				.form(name: "message", value: body)
 				.authorize(auth: authorization)
-				.post()
-				.then { response in
-					try response.validateBody(bodyType: String.self)
-				}
+
+		for param in params {
+			builder = builder.form(name: param.name, value: param.value)
+		}
+
+		return builder
+			.post().then { response in
+				try response.validateBody(bodyType: String.self)
+			}
 	}
 
 	public func checkEmailStatus(id: String) -> Promise<String> {
